a82116021d74b62604fbdff923b57b86118238bc
Clayton Oliveira <clayton.will08@gmail.com>
2025-12-02 22:09:52 -0300

    - Add Config  Program (Cors) - Configuração SeedData

--- WasmMP2.Api/Data/SeedData.cs
+++ WasmMP2.Api/Data/SeedData.cs
@@ -1,6 +1,50 @@
-﻿namespace WasmMP2.Api.Data
+﻿using WasmMP2.Api.Models;
+
+namespace WasmMP2.Api.Data
 {
-    public class SeedData
+    public static class SeedData
     {
+        public static void Initialize(AppDbContext context)
+        {
+            //cria base de dados e tabelas se elas ainda não existem
+            context.Database.EnsureCreated();
+
+            //se tem produtos, ele não faz nada
+            if (context.Products.Any()) return;
+
+            var catBurger = new Category { Name = "Lanches" };
+            var catDrink = new Category { Name = "Bebidas" };
+            var catDessert = new Category { Name = "Sobremessa" };
+            context.Categories.AddRange(catBurger, catDrink);
+
+            context.Products.AddRange(
+                new Product
+                {
+                    Name = "xBurger",
+                    Price = 25.9m,
+                    ImageUrl = "https://via.placeholder.com/300x200?text=XBurger",
+                    IsFavorite = true,
+                    Category = catBurger
+                },
+                new Product
+                {
+                    Name = "Coca-Cola",
+                    Price = 7.0m,
+                    ImageUrl = "https://via.placeholder.com/300x200?text=XBurger",
+                    IsFavorite = true,
+                    Category = catDrink
+                },
+                new Product
+                {
+                    Name = "Pudim",
+                    Price = 10.9m,
+                    ImageUrl = "https://via.placeholder.com/300x200?text=batata+frita",
+                    IsFavorite = true,
+                    Category = catDessert
+                }
+            );
+
+            context.SaveChanges();
+        }
     }
 }
--- WasmMP2.Api/Program.cs
+++ WasmMP2.Api/Program.cs
@@ -1,41 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using WasmMP2.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//dbContext com sqlite
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var cs = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=wasmmp2.db";
+    options.UseSqlite(cs);
+});
+
+//CORS para ambiente de dev
+var corsPolicyName = "AllowWasmClient";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsPolicyName, policy =>
+    {
+        //ajustar as URLs conforme a porta Client
+        policy.WithOrigins("http://localhost:5148", "https://localhost:5148").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Aplicar o seedService na inicialização
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    SeedData.Initialize(db);
+}
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+//Habilita o CORS   
+app.UseCors(corsPolicyName);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+//GET /api/categories
+app.MapGet("/api/categories", async (AppDbContext db) => await db.Categories.AsNoTracking().ToListAsync());
+
+//GET /api/products
+app.MapGet("/api/products", async (AppDbContext db) => await db.Products.Include(p => p.Category).AsNoTracking().ToListAsync());
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
